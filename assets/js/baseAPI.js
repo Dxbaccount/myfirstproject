cda76ed190e5dc7ec0fed230ba45a58c89a8f610
dsl <2394633826@qq.com>
2022-07-20 19:30:47 +0800

    1

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,12 @@
 // 通过这个函数可以拿到为 ajax 提供的配置对象
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    // 统一为有权限的接口设置请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    
 })
